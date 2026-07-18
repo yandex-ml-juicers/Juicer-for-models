b94ebfa74039a37ebb504e3fe9c0e2a857df9eb8
Илья Каплун <retro.fert@mail.ru>
2026-07-18 17:33:09 +0000

    exp: add loading imagenet datasets

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -1,37 +1,44 @@
 """Фабрики датасетов. Вызываются через hydra.utils.instantiate из cfg.data.dataset
 с дозаполнением аргументов train= и transform= в рантайме (см. loaders.py)."""
 
+import zipfile
 import random
 from collections import defaultdict
+from pathlib import Path
 from typing import Callable
 
 import torchvision
 from datasets import DownloadConfig, concatenate_datasets, load_dataset
+from huggingface_hub import hf_hub_download
 from hydra.utils import to_absolute_path
 from torch.utils.data import Dataset
 
 
 class HFImageNet(Dataset):
+    IMAGENET100_REPO = "asafaa/imagent100"
+
     def __init__(
         self,
         root: str,
+        repo_path: str,
         split: str,
-        transform: Callable = None,
+        transform: Callable | None = None,
         download: bool = True,
     ) -> None:
         if split not in {"train", "validation"}:
-            raise ValueError(
-                "split должен быть 'train' или 'validation'"
-            )
+            raise ValueError("split должен быть 'train' или 'validation'")
+
+        root_path = Path(to_absolute_path(root))
 
         self.dataset = load_dataset(
-            "ILSVRC/imagenet-1k",
+            repo_path,
             split=split,
-            cache_dir=to_absolute_path(root),
+            cache_dir=str(root_path),
             download_config=DownloadConfig(
                 local_files_only=not download,
             ),
         )
+
         self.transform = transform
 
     def __len__(self) -> int:
@@ -73,19 +80,19 @@ def cifar10(
         transform=transform,
     )
 
-def imagenet_1k(
+
+def imagenet(
     root: str,
+    repo_path: str,
     train: bool,
     transform: Callable | None = None,
     download: bool = True,
 ) -> Dataset:
-    """ImageNet-1K dataset.
+    """Function for loading imagenet datasets."""
 
-    The official ImageNet-1K dataset is used for image classification tasks.
-    """
-    
     return HFImageNet(
         root=root,
+        repo_path=repo_path,
         split="train" if train else "validation",
         transform=transform,
         download=download,
