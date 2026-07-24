682577317c38bf22745f588fcd70ba4924359e67
prel-c <mkhail381230@gmail.com>
2026-07-24 00:14:31 +0000

    fix: bugs for tinyvit return

--- src/models/factory.py
+++ src/models/factory.py
@@ -200,6 +200,8 @@ def tinyvit(
 
         model.load_state_dict(state_dict, strict=True)
 
+    return model
+
 def shufflenet_v2_x1_0(num_classes: int = 100) -> nn.Module:
     return tv_models.shufflenet_v2_x1_0(
         num_classes=100,
