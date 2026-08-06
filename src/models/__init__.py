5b7c223a959f2e2c427c438c73da3b94123c2d8a
Mikhail Obukhov <mikhail381230@gmail.com>
2026-08-07 02:50:12 +0300

    feat: add new implementation for U-Net

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,30 @@
 from src.models.adapters import ChannelAdapters
-from src.models.factory import cifar_resnet18, from_detectors, from_torch_hub
-from src.models.feature_extractor import FeatureExtractor
+from src.models.factory import (
+    cifar_resnet18,
+    from_detectors,
+    from_torch_hub,
+    segformer_for_segmentation,
+    unet_for_segmentation,
+)
+from src.models.feature_extractor import FeatureExtractor, unwrap_model
+from src.models.feature_taps import STAGE_TAP_STRIDES, STAGE_TAPS, FeatureTaps
+from src.models.segformer import SEGFORMER_VARIANTS, SegFormer
+from src.models.unet import UNET_VARIANTS, UNet
 
 __all__ = [
     "ChannelAdapters",
     "FeatureExtractor",
+    "FeatureTaps",
+    "STAGE_TAPS",
+    "STAGE_TAP_STRIDES",
+    "SegFormer",
+    "SEGFORMER_VARIANTS",
+    "UNET_VARIANTS",
+    "UNet",
     "cifar_resnet18",
     "from_detectors",
     "from_torch_hub",
+    "segformer_for_segmentation",
+    "unet_for_segmentation",
+    "unwrap_model",
 ]
