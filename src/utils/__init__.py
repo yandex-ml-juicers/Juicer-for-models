4465fa44ec6ef451891c63f7a41a88e36e836a13
Mikhail Obukhov <mikhail381230@gmail.com>
2026-08-07 02:54:06 +0300

    feat: add base functions for factory

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,10 @@
+from src.utils.checkpoints import (
+    DEFAULT_WEIGHTS_DIR,
+    download_file,
+    extract_state_dict,
+    load_checkpoint_into,
+    resolve_weights_dir,
+)
 from src.utils.device import resolve_device
 from src.utils.logger import MetricsHistory, get_logger
 from src.utils.metrics import AverageMeter, accuracy
@@ -6,11 +13,16 @@ from src.utils.prediction_postprocessor import prediction_postprocessor
 
 __all__ = [
     "AverageMeter",
+    "DEFAULT_WEIGHTS_DIR",
     "MetricsHistory",
     "accuracy",
+    "download_file",
+    "extract_state_dict",
     "get_logger",
+    "load_checkpoint_into",
     "make_generator",
     "resolve_device",
+    "resolve_weights_dir",
     "seed_everything",
     "seed_worker",
     "prediction_postprocessor"
