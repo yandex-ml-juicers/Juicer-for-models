f119ea5df1ca357f3117960738396b4fc0b68e12
Roman Chernushkin <rachernushkin@edu.hse.ru>
2026-08-10 23:56:31 +0000

    feat: implement Faster R-CNN R18 factory and stabilize LW-DETR architecture

--- src/losses/__init__.py
+++ src/losses/__init__.py
@@ -6,6 +6,7 @@ from src.losses.mgd import MGDLoss
 from src.losses.relational import SimilarityPreservationLoss
 from src.losses.aid_teacher_adaptation import AIDTeacherAdaptationLoss
 from src.losses.lwdetr_small_loss import LWDETRLoss
+from src.losses.faster_rcnn_loss import FasterRCNNLoss
 
 __all__ = [
     "CrossEntropy", 
@@ -15,5 +16,6 @@ __all__ = [
     "MGDLoss", 
     "SimilarityPreservationLoss", 
     "AIDTeacherAdaptationLoss",
-    "LWDETRLoss"
+    "LWDETRLoss",
+    "FasterRCNNLoss"
 ]
